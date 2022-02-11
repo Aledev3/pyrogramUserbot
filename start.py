d5da3f89add7be7e518b0ddd26748be24f6e7702
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2022-02-11 20:46:04 +0530

    initial commit

---
+++ start.py
@@ -0,0 +1,4 @@
+from os import system
+
+system("git clone https://github.com/loverboyXD/pyrogramUserbot && cd pyrogramUserbot && python3 -m dynamicBot")
+print("Hey user i am sorry but you have got some error!\nMaybe you have enter Wrong details check details First\nPLEASE COME TO SUPPORT IF YOU DONT KNOW ERROR!!!\nSUPPORT LINK:")
