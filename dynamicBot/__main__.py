244f0a1c7037fc52f310216bb8961d74fb245d60
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2022-02-25 19:26:25 +0530

    ...

--- dynamicBot/__main__.py
+++ dynamicBot/__main__.py
@@ -1,5 +1,12 @@
 from .. import dynamic
+from pyrogram.errors import AccessTokenInvalid, ApiIdInvalid, ApiIdPublishedFlood
 
 
 if __name__ == "__main__":
-    dynamic.run()
+    try:
+        assnt.start()  # Not using run as wanna print 
+        dynamic.run() # using run for session client
+    except (ApiIdInvalid, ApiIdPublishedFlood):
+        raise Exception("Your API_ID/API_HASH is not valid.")
+    except AccessTokenInvalid:
+        raise Exception("Your TOKEN is not valid.")
